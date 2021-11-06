1edd28c08363f418fdbcbcc5b6d3627e19b10db2
D1074181045 <D1074181045@gm.lhu.edu.tw>
2021-11-07 01:51:02 +0800

    [add] save last position. asset distribute

---
+++ js/main.js
@@ -0,0 +1,38 @@
+function transfer_url(to_name) {
+	let mystic_num = document.getElementById("number").value;
+	
+	if (!mystic_num) {
+		alert('尚未輸入');
+		return;
+	}
+		
+	let url;
+	switch (to_name) {
+		case 'nhentai':
+			url = "https://nhentai.net/g/".concat(mystic_num);
+			break;
+		case 'wnacg':
+			url = "https://www.wnacg.org/photos-index-aid-".concat(mystic_num, ".html");
+			break;
+		case 'pixiv':
+			url = "https://www.pixiv.net/artworks/".concat(mystic_num);
+			break;
+	}
+	
+	if (url) window.open(url);
+}
+
+var mySwiper = new Swiper( ".swiper-container", {
+    spaceBetween: 30,
+	direction: "horizontal",
+	navigation: {
+		nextEl: ".swiper-button-next", // 上一頁按鈕物件
+		prevEl: ".swiper-button-prev", // 下一頁按鈕物件
+	},
+	initialSlide: localStorage.position,
+	on: {
+		slideChangeTransitionEnd: function () {
+			localStorage.position = this.activeIndex;
+		},
+	}
+})
